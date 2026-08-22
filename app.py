224977e033011c67c1cd4a84fdfaa6bdfcef5c35
Manu Arunmozhy <manuarunmozhy125@gmail.com>
2026-08-22 12:20:49 +0530

    Hour 4: Added employee profile and attendance

--- app.py
+++ app.py
@@ -3,13 +3,11 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 
-# LOGIN PAGE
 @app.route("/")
 def login():
     return render_template("login.html")
 
 
-# PROCESS LOGIN
 @app.route("/login", methods=["POST"])
 def handle_login():
 
@@ -22,16 +20,24 @@ def handle_login():
         return redirect("/admin")
 
 
-# EMPLOYEE DASHBOARD
 @app.route("/employee")
 def employee_dashboard():
     return render_template("employee_dashboard.html")
 
 
-# ADMIN DASHBOARD
 @app.route("/admin")
 def admin_dashboard():
     return render_template("admin_dashboard.html")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
+@app.route("/attendance")
+def attendance():
+    return render_template("attendance.html")
 
 
 if __name__ == "__main__":
